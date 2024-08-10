ea1d6491b5dfeb11269d5928bd8f43fd42b4509d
MartinMFM <martin.mf665@gmail.com>
2024-08-09 22:56:41 -0700

    adding header and home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+export const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/martin.floresm"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/martin-flores-medina-b7840a322/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/MartinMFM"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
