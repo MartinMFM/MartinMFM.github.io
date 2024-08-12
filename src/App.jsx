1da96069e7c29fd3b071499162e816ab3ff2aa13
MartinMFM <martin.mf665@gmail.com>
2024-08-11 18:43:48 -0700

    about component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 
 import { Header } from "./components/header/Header";
 import { Home } from "./components/home/Home";
+import { About } from "./components/about/About";
 export const App = () => {
   return (
     <>
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
