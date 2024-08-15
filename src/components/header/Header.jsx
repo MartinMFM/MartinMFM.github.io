feb32b5cd9258e5778c8d1c211da7531efbf0153
MartinMFM <martin.mf665@gmail.com>
2024-08-14 23:40:49 -0700

    contact component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -31,11 +31,11 @@ export const Header = () => {
               </a>
             </li>
 
-            <li className="nav__item">
+            {/* <li className="nav__item">
               <a href="#services" className="nav__link">
                 <i className="uil uil-briefcase-alt nav__icon"></i> Services
               </a>
-            </li>
+            </li> */}
 
             <li className="nav__item">
               <a href="#portfolio" className="nav__link">
